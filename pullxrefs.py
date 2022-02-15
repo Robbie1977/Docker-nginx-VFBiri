aa294b9af6e6b24f3dbec2597aa315961587efd4
Rob Court <robbie.court@gmail.com>
2022-02-15 21:12:36 +0000

    typo fix

--- pullxrefs.py
+++ pullxrefs.py
@@ -8,7 +8,6 @@ for line in map[0]:
     output += "\n" + "".join(line)
 
 map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'jrc_slide_code_api'}) WHERE exists(r.accession) WITH * ORDER BY r.accession Asc, a.short_form Desc RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
-output = ""
 for line in map[0]:
   if not line[1] in output:
     output += "\n" + "".join(line)
