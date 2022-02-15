784d73a7a97d357e252f5abd9caa1005df920f46
Rob Court <robbie.court@gmail.com>
2022-02-15 20:06:52 +0000

    secondary sorting

--- pullxrefs.py
+++ pullxrefs.py
@@ -1,7 +1,7 @@
 from vfb_connect.cross_server_tools import VfbConnect
 vc = VfbConnect()
 
-map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) WITH * ORDER BY r.accession Asc RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
+map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) WITH * ORDER BY r.accession Asc, a.short_form Asc RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
 output = ""
 for line in map[0]:
   output += "\n" + "".join(line)
