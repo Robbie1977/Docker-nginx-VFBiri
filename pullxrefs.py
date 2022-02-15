0961c26e99ca8d66679570134cc07c7f9d6dfcc5
Rob Court <robbie.court@gmail.com>
2022-02-15 18:25:39 +0000

    joining lines

--- pullxrefs.py
+++ pullxrefs.py
@@ -3,4 +3,4 @@ vc = VfbConnect()
 
 map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
 
-print(map)
+print('\n'.join(map))
