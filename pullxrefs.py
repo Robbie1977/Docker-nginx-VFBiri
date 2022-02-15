a501d30a84ed7a35bc791125e042dfcc2719f090
Rob Court <robbie.court@gmail.com>
2022-02-15 21:31:54 +0000

    logic fix

--- pullxrefs.py
+++ pullxrefs.py
@@ -3,12 +3,12 @@ vc = VfbConnect()
 
 map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) WITH * ORDER BY r.accession Asc, a.short_form Desc RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
 output = ""
-for line in map[0]:
+for line.replace(" ","%20") in map[0]:
   if not line[1] in output:
     output += "\n" + "".join(line).replace(line[1],line[1].replace(" ","%20"))
 
 map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'jrc_slide_code_api'}) WHERE exists(r.accession) WITH * ORDER BY r.accession Asc, a.short_form Desc RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
-for line in map[0]:
+for line.replace(" ","%20") in map[0]:
   if not line[1] in output:
     output += "\n" + "".join(line).replace(line[1],line[1].replace(" ","%20"))
     
