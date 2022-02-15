d3ec65fc4fdf52885d866bcc622ecbb08cc5d798
Rob Court <robbie.court@gmail.com>
2022-02-15 18:41:22 +0000

    ordering list

--- pullxrefs.py
+++ pullxrefs.py
@@ -1,7 +1,7 @@
 from vfb_connect.cross_server_tools import VfbConnect
 vc = VfbConnect()
 
-map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
+map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map ORDER BY map Asc"])[0]['data'][0]['row']
 output = ""
 for line in map[0]:
   output += "\n" + "".join(line)
