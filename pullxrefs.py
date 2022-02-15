5680649e92e936dacad8ff793e6412927468eef2
Rob Court <robbie.court@gmail.com>
2022-02-15 18:38:51 +0000

    writing to file

--- pullxrefs.py
+++ pullxrefs.py
@@ -4,6 +4,7 @@ vc = VfbConnect()
 map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
 output = ""
 for line in map[0]:
-  output += "/n" + "".join(line)
+  output += "\n" + "".join(line)
 
-print(output)
+with open('neuronbridge.map', 'w') as the_file:
+    the_file.write(output + "\n")
