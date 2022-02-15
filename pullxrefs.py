b42cffcc03647c765859d6a89107e4eb7b25a57a
Rob Court <robbie.court@gmail.com>
2022-02-15 18:29:13 +0000

    merging

--- pullxrefs.py
+++ pullxrefs.py
@@ -2,5 +2,8 @@ from vfb_connect.cross_server_tools import VfbConnect
 vc = VfbConnect()
 
 map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'neuronbridge'}) WHERE exists(r.accession) RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
+output = ""
+for line in map:
+  output += "/n" + "".join(line)
 
-print('\n'.join(map))
+print(output)
