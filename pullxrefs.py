754ac91a14828b97daf98802fb6341577ae78691
Rob Court <robbie.court@gmail.com>
2022-02-15 20:55:49 +0000

    adding slide codes

--- pullxrefs.py
+++ pullxrefs.py
@@ -7,5 +7,12 @@ for line in map[0]:
   if not line[1] in output:
     output += "\n" + "".join(line)
 
+map = vc.nc.commit_list(["MATCH (a)-[r]->(b:Site {short_form:'jrc_slide_code_api'}) WHERE exists(r.accession) WITH * ORDER BY r.accession Asc, a.short_form Desc RETURN DISTINCT collect( '    rewrite ^/xref/' + b.short_form + '/' + r.accession + ' https://v2.virtualflybrain.org/reports/' + a.short_form + ' last;') as map"])[0]['data'][0]['row']
+output = ""
+for line in map[0]:
+  if not line[1] in output:
+    output += "\n" + "".join(line)
+    
 with open('neuronbridge.map', 'w') as the_file:
     the_file.write(output + "\n")
+    
