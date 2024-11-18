5eb9d03e1e05d80ac02ba15e9284f56c69e7f4bb
Rob Court <robbie.court@gmail.com>
2024-11-18 15:23:00 +0000

    swapping to kb

--- pullxrefs.py
+++ pullxrefs.py
@@ -8,7 +8,7 @@ if os.path.exists('neuronbridge.map'):
         initial_lines = sum(1 for line in file)
 print(f"Initial number of lines in neuronbridge.map: {initial_lines}")
 
-vc = VfbConnect()
+vc = VfbConnect(endpoint='http://kb.virtualflybrain.org', usr='neo4j', pwd='vfb')
 
 # Query for matches with non-empty accession filter and excluding deprecated nodes
 query = """
